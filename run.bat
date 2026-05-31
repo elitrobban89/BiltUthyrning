7bd8b535ff92a20e450e085796e86049cf155aed
elitrobban89 <eliterobban@gmail.com>
2026-05-31 09:23:11 +0200

    Add run.bat with correct JavaFX SDK path for launching the app

---
+++ run.bat
@@ -0,0 +1,3 @@
+@echo off
+java -jar target\car-rental-1.0-SNAPSHOT.jar --module-path "C:\Users\robba\Downloads\openjfx-26.0.1_windows-x64_bin-sdk\javafx-sdk-26.0.1\lib" --add-modules javafx.controls,javafx.fxml
+pause
